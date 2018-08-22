4d5af28fa924ffcd9bcbdd412a393acd4ae16961
gaofeng wang <gaofeng.wang@sandisk.com>
2018-08-22 13:17:30 +0800

    initiate

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MyPy',
+    version='0.0',
+    packages=['MyPy'],
+    url='',
+    license='MIT',
+    author='12706',
+    author_email='',
+    description=''
+)
---
+++ src/MyPy/mod1.py
@@ -0,0 +1,5 @@
+def sayhello():
+    print('hello world')
+
+if __name__ == "__main__":
+    sayhello()
